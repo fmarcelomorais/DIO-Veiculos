b8d1e317d98e9cdbbf941dd74baf8cd4c830e139
fmarcelomorais <marcelomorais.program@gmail.com>
2024-08-20 15:44:34 -0300

    feat: Deleta arquivos duplicados

--- Domain/DTOs/AdministratorDTO.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DIO_Veiculos.Domain.DTOs
-{
-    public record AdministratorDto(string Name, string Email, string Password);
-}
